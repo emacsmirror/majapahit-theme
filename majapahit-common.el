4c48dd287581186f71367bb3dc5fdd305790c632
Alex Frank Adhyatma <franksn@openmailbox.org>
2015-08-30 04:55:53 +0700

    fix comment, again

--- majapahit-common.el
+++ majapahit-common.el
@@ -68,7 +68,7 @@
         (comment       (if (eq variant 'dark)  "#999999"  "#bababa" ))
         (comment-bg    (if (eq variant 'dark)  "#2c2c25"  "#fafaf6" ))
         (func          (if (eq variant 'dark)  "#d99174"  "#cf8164" ))
-        (str           (if (eq variant 'dark)  "#babea5"  "#abaf96" ))
+        (str           (if (eq variant 'dark)  "#babea5"  "#a1a58e" ))
         (type          (if (eq variant 'dark)  "#51808f"  "#4b97a8" ))
         (comp          (if (eq variant 'dark)  "#e47376"  "#db4764" ))
         (var           (if (eq variant 'dark)  "#b1b59c"  "#a5947b" ))
