d70e5d6bc4823cbba3a397bbb0edb21a02da39e7
Alex Frank Adhyatma <franksn@openmailbox.org>
2015-09-14 01:17:13 +0700

    last one before the Milan derby

--- majapahit-common.el
+++ majapahit-common.el
@@ -107,7 +107,7 @@
         (violet        (if (eq variant 'dark)  "#635363"  "#5a6096" ))
         (red           (if (eq variant 'dark)  "#c5617a"  "#d25f76" ))
         (active1       (if (eq variant 'dark)  "#20201a"  "#e8e8dc" ))
-        (active2       (if (eq variant 'dark)  "#63635a"  "#e5e0d7" ))
+        (active2       (if (eq variant 'dark)  "#63635a"  "#dbdbcb" ))
         (inactive      (if (eq variant 'dark)  "#45b0d3"  "#7eacac" ))
         (m-line-brdr   (if (eq variant 'dark)  "#4c4c42"  "#e3e3da" ))
         (org-block-bg  (if (eq variant 'dark)  "#232528"  "#d9e6dd" ))
